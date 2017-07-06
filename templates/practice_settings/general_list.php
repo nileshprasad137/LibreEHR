a1eba270807bd8e3248e07a748e029c124383263
Nilesh Prasad <prasadnilesh96@gmail.com>
2017-07-07 00:12:08 +0530

    added nav-tabs for practice settings

--- templates/practice_settings/general_list.php
+++ templates/practice_settings/general_list.php
@@ -7,28 +7,51 @@
 <script type="text/javascript" src="<?php echo $GLOBALS['rootdir'] . '/../library/js/jquery.1.3.2.js';?>"></script>
 <script type="text/javascript" src="<?php echo $GLOBALS['rootdir'] . '/../library/js/common.js';?>"></script>
 </head>
-<body class="body_top">
+<!--
+<script>
+    $('#myTab a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+</script>
+-->
+<body>
 
 <div>
-    <b><?php echo xl("Practice Settings");?></b>
+    <h6 style="padding:5px"><b><?php echo xl("Practice Settings");?></b></h6>
 </div>
 
-<div>
-    <div class="small">
-        <a href="<?php echo $this->top_action;?>pharmacy&action=list"><?php echo xl("Pharmacies");?></a> |
-        <a href="<?php echo $this->top_action;?>insurance_company&action=list"><?php echo xl("Insurance Companies");?></a> |
-        <a href="<?php echo $this->top_action;?>insurance_numbers&action=list"><?php echo xl("Insurance Numbers");?></a> |
-        <a href="<?php echo $this->top_action;?>x12_partner&action=list"><?php echo xl("X12 Partners");?></a> |
-        <a href="<?php echo $this->top_action;?>document&action=queue"><?php echo xl("Documents");?></a> |
-        <a href="<?php echo $this->top_action;?>hl7&action=default"><?php echo xl("HL7 Viewer");?></a>
+<div class="col-md-12 col-xs-12 col-lg-12">
+    <div class="row">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="col-md-2 col-xs-4">
+                <a role="tab" data-toggle="tab" href="<?php echo $this->top_action;?>pharmacy&action=list"><?php echo xl("Pharmacies");?></a>
+            </li>
+            <li class="col-md-2 col-xs-4" data-toggle="tab">
+                <a role="tab" data-toggle="tab" href="<?php echo $this->top_action;?>insurance_company&action=list"><?php echo xl("Insurance Companies");?></a>
+            </li>
+            <li class="col-md-2 col-xs-4" data-toggle="tab">
+                <a role="tab" data-toggle="tab" href="<?php echo $this->top_action;?>insurance_numbers&action=list"><?php echo xl("Insurance Numbers");?></a>
+            </li>
+            <li class="col-md-2 col-xs-4" data-toggle="tab">
+                <a role="tab" data-toggle="tab" href="<?php echo $this->top_action;?>x12_partner&action=list"><?php echo xl("X12 Partners");?></a>
+            </li>
+            <li class="col-md-2 col-xs-4" data-toggle="tab">
+                <a role="tab" data-toggle="tab" href="<?php echo $this->top_action;?>document&action=queue"><?php echo xl("Documents");?></a>
+            </li>
+            <li class="col-md-2 col-xs-4" data-toggle="tab">
+                <a role="tab" data-toggle="tab" href="<?php echo $this->top_action;?>hl7&action=default"><?php echo xl("HL7 Viewer");?></a>
+            </li>
+        </ul>        
     </div>
     
     <br/>
-    <div class="section-header">
-        <b><?php echo $this->action_name;?></b>
+    <div>
+        <h5><b><?php echo $this->action_name;?></b></h5>        
     </div>
-    <div class="tabContainer">
-        <div class="tab current">
+    
+    <div class="tab-content">
+        <div class="tab-pane fade active in">
             <?php echo $this->display_action;?>
         </div>
     </div>
