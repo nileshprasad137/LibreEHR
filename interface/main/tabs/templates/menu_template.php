3b0f0f2bb55820ab972c4b248b3cdd6cd1f17a8a
Nilesh Prasad <prasadnilesh96@gmail.com>
2017-07-30 23:57:04 +0530

    dropdown on hover completed

--- interface/main/tabs/templates/menu_template.php
+++ interface/main/tabs/templates/menu_template.php
@@ -1,5 +1,21 @@
+<style>
+    .nav>li>a{
+        color: #31b0d5;
+        cursor: pointer;
+        font-size: 14px;
+    }
+    .nav>li>a:hover > ul{
+        display:none;
+    }
+    .icon-bar{
+        color:white;
+        background: black;
+    }
+    
+
+</style>
 <script type="text/html" id="menu-action">
-    <a href="#" data-bind="text:label,click: menuActionClick,css: {menuDisabled: ! enabled()}"></a>
+    <a data-bind="text:label,click: menuActionClick,css: {menuDisabled: ! enabled()}"></a>
 </script>
 <script type="text/html" id="menu-header">
     <!--
@@ -10,9 +26,11 @@
         <ul>
     </div>
     -->
-    <a href="#" data-bind="text:label" class="dropdown-toggle " data-toggle="dropdown"> <b class="caret"></b></a>
+    <a data-bind="text:label" class="dropdown-toggle " data-toggle="dropdown"> <b class="caret"></b></a>
     <ul class="dropdown-menu" data-bind="foreach: children">
-        <li data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }, css:{name:header ? 'dropdown':''}"></li>
+        <!--<li data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></li>-->
+        <li data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></li>
+
     </ul>
 </script>
 <script type="text/html" id="menu-template">
@@ -22,15 +40,24 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Brand</a>
+        </button>        
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav" data-bind="foreach: menu">
-          <li data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }, css:{name:header ? 'dropdown':''}"></li>            
+          <li class="dropdown" data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></li>            
         </ul>
     </div>
 </script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.nav li.dropdown').hover(function() {
+        $(this).addClass('open');
+    }, function() {
+        $(this).removeClass('open');
+    });
+});
+</script>
+
 
